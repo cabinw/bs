0d5c91c10c13f9bdee134b3684cbcfd1c2357438
cabinw <cabinw@gmail.com>
2012-09-24 14:03:29 +0800

    remove useless code

--- app.py
+++ app.py
@@ -2,8 +2,6 @@
 import tornado.ioloop
 from tornado.options import define, options, logging
 import tornado.web
-import unittest
-from jinja2 import Environment, PackageLoader
 import os
 define("port", default=8888, help="run on the given port", type=int)
 
@@ -15,56 +13,6 @@ server_settings = {
     "xheaders" : True,
 
 }
-
-class Test(unittest.TestCase):
-    
-    consumer_key= "2886103344"
-    consumer_secret ="87e807bec7a2481728646d3f1c1459f1"
-    
-    def __init__(self):
-            """ constructor """
-    
-    def getAtt(self, key):
-        try:
-            return self.obj.__getattribute__(key)
-        except Exception, e:
-            print e
-            return ''
-        
-    def getAttValue(self, obj, key):
-        try:
-            return obj.__getattribute__(key)
-        except Exception, e:
-            print e
-            return ''
-        
-    def auth(self):
-        self.auth = OAuthHandler(self.consumer_key, self.consumer_secret)
-        auth_url = self.auth.get_authorization_url()
-        print 'Please authorize: ' + auth_url
-        verifier = raw_input('PIN: ').strip()
-        self.auth.get_access_token(verifier)
-        self.api = API(self.auth)
-     
-    def setToken(self, token, tokenSecret):
-        self.auth = OAuthHandler(self.consumer_key, self.consumer_secret)
-        self.auth.setToken(token, tokenSecret)
-	self.api = API(self.auth)
-    
-    def update(self, message):
-        message = message.encode("utf-8")
-        status = self.api.update_status(status=message, lat="22.512556954051437", long="114.169921875")
-        self.obj = status
-        id = self.getAtt("id")
-        text = self.getAtt("text")
-        print "update---"+ str(id) +":"+ text
-        
-    def destroy_status(self, id):
-        status = self.api.destroy_status(id)
-        self.obj = status
-        id = self.getAtt("id")
-        text = self.getAtt("text")
-        print "update---"+ str(id) +":"+ text
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
